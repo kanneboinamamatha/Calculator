f08721d9aa4fabf9e1aa7c8d8799e6c13b40e4d2
istar <istar@192.168.100.3>
2017-05-25 10:39:04 +0530

    relational operator

---
+++ src/com/onlinecalculator/calculator/calctypes/relationaloptr.java
@@ -0,0 +1,19 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.Scanner;
+public class relationaloptr {
+public static void main (String args[])
+{
+	int a,b;
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter first number");
+	a=s.nextInt();
+	System.out.println("enter second number");
+	b=s.nextInt();
+	System.out.println("a<b"+(a<b));
+	System.out.println("a>b"+(a>b));
+	System.out.println("a<=b"+(a<=b));
+	System.out.println("a>=b"+(a>=b));
+	System.out.println("a==b"+(a==b));
+	System.out.println("a!=b"+(a!=b));
+}
+}
