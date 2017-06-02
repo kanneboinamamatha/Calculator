87fe05be6d87feef4a101ee7d260c13b5d21a337
ADMIN <ADMIN@DESKTOP-31G8HVG>
2017-06-02 06:07:42 -0700

    Main program-mamatha

--- src/com/onlinecalculator/calculator/maincalc/MainPgm.java
+++ src/com/onlinecalculator/calculator/maincalc/MainPgm.java
@@ -5,27 +5,49 @@ import java.util.Scanner;
 import com.onlinecalculator.calculator.calctypes.*;
 
 public class MainPgm {
-public static void main(String args[]){
-        char type;
-        type=getType();
-        if(type=='a')
-        {
-        	
-        }
-        if(type=='b')
-        {
-       		
-       	}
-        	
-        	if(type=='c')
-        	{
-        		
-        	}
- 
+public static void main(String args[])
+{
+	int type;
+        type=disp();
+       
+ if(type==1)
+ {
+	 BasicCalc calc=new BasicCalc();
+	 calc.menu();
+	calc.userInputs();
+	calc.performOperation();  
     }
-        
-    static char getType()
+     if(type==2)
+     {
+    	 ScientificCalc scalc=new ScientificCalc();
+    	 scalc.menu();
+    	scalc.userInputs();
+    	scalc.performOperation();  
+        } 
+     if(type==3)
+     {
+    	 StatisticCalc scalc=new StatisticCalc();
+    	 scalc.menu();
+    	 scalc.userInputs();
+     	scalc.performOperation();  
+     	
+    	 
+     }
+}
+
+    static int disp()
     {
-    	
+    	int ch;
+    	Scanner sc=new Scanner(System.in);
+	  	System.out.println("******CALCULATOR*********");
+	  	System.out.println("*********1.BASIC CALCULATOR***********");
+	  	System.out.println("*********2.SCIENTIFIC CALCULATOR**********");
+	  	System.out.println("***********3.STATISTICAL CALCULATOR**********");
+	  	System.out.println("******************");
+	  	System.out.println("enter your choice:");
+	  	ch=sc.nextInt();
+	  	return(ch);
     }
 }
+
+    
